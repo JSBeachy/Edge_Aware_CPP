ca7f84e59e07f498c91b4aeb5a8226e828ce2384
michaelkaiwi <137839447+michaelkaiwi@users.noreply.github.com>
2024-08-22 16:34:43 -0700

    visualization of plane slicing

--- boundingbox.py
+++ boundingbox.py
@@ -17,7 +17,7 @@ for i in range(plane_nums):
 print(plane_int)
 '''
 plane=o3d.io.read_triangle_mesh('plane_segments\plane_segment_8_mesh.stl')
-#o3d.visualization.draw_geometries([plane])
+o3d.visualization.draw_geometries([plane])
 
 bounding_box=plane.get_oriented_bounding_box()
 
@@ -28,7 +28,16 @@ bounding_box.color=[1,0,0]
 #min_bounding_box.color=(0,1,0)
 #o3d.visualization.draw_geometries([plane,bounding_box,min_bounding_box])
 
-#o3d.visualization.draw_geometries([plane,bounding_box])
+
+frame=o3d.geometry.TriangleMesh.create_coordinate_frame(size=100)
+transformation=np.eye(4)
+transformation[:3,:3]=bounding_box.R
+transformation[:3, 3]=bounding_box.center
+frame.transform(transformation)
+o3d.visualization.draw_geometries([plane,bounding_box,frame])
+
+
+
 bblen=list(bounding_box.extent)
 primary_axis_index=bblen.index(max(bblen))
 popped_bblen=bblen[0:primary_axis_index]+bblen[primary_axis_index+1:]
@@ -59,14 +68,16 @@ probe_pass_area=secondary_axis_length-probe_width
 # num passes is probe_pass_area/probe_width rounded up for full, slightly overlapping coverage
 #TODO Can be used to enforce certain amount of overlap as well (future work)
 
-num_interior_passes= -(-probe_pass_area//probe_width)
-scan_lane_width=probe_pass_area/num_passes
+num_interior_passes= -int(-probe_pass_area//probe_width)
+scan_lane_width=probe_pass_area/num_interior_passes
 print(probe_width/2+scan_lane_width/2+scan_lane_width+scan_lane_width+scan_lane_width/2+probe_width/2,secondary_axis_length)
 j=[i for i in range(num_interior_passes+2)]
 off=0
 for i in j:
+    bbedge=cent-secondary_axis/2
+    #TODO add bbedge as refernce point instead of offset+center
     if j.index(i)==0:
-        off=-secondary_axis_length/2
+        off=-secondary_axis_length/2+probe_width/2
     elif j.index(i)==1 or j.index(i)==j[-1]:
         off=off+scan_lane_width/2
     else:
@@ -76,7 +87,7 @@ for i in j:
         for v in np.linspace(-plane_size/2,plane_size/2,int(100/2)):
             
             #creates points on plane along primary axis with tertiary for depth
-            point_on_plane=offset+cent+u*rot[:,primary_axis_index]+v*rot[:,tertiary_axis_index]
+            point_on_plane=offset+cent+ u*rot[:,primary_axis_index]+v*rot[:,tertiary_axis_index]
             plane_points.append(point_on_plane)
     plane_pcd=o3d.geometry.PointCloud()
     plane_pcd.points=o3d.utility.Vector3dVector(plane_points)
