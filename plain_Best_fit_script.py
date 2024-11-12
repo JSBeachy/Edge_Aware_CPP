398cc3267a97f3fc4b8c45cc307f7aab55af9f10
michaelkaiwi <137839447+michaelkaiwi@users.noreply.github.com>
2024-11-12 10:41:44 -0800

    updated the plain script to match the current standing of the oop one

--- plain_Best_fit_script.py
+++ plain_Best_fit_script.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial import ConvexHull
 import itertools
+import math
 from PCAClass import PCABounding, Best_Fit_CPP
 
 def distance_theshold(new_point,cornerpoints):
@@ -17,7 +18,7 @@ def linearity_score(vec1,vec2):
     cos_theta= np.dot(vec1,vec2) / (np.linalg.norm(vec1)*np.linalg.norm(vec2))
     return abs(cos_theta)
 
-def fitting(len_my_list, edge_list):
+def splitting(len_my_list, edge_list, hull_vertices_list):
     #use list comprehension and sorting to order the list index
     direction=1
     index=sorted([hull_vertices_list.index(i) for i in edge_list])
@@ -28,6 +29,35 @@ def fitting(len_my_list, edge_list):
         direction=-1
     group=[hull_vertices_list[(index[0] + i*direction)] for i in range(length+1)]
     return np.asarray(group)
+
+def fit_line_3d(points):
+    # Calculate the centroid of the points
+    centroid = np.mean(points, axis=0)
+    # Use Singular Value Decomposition (SVD) to find the principal direction
+    _, _, vh = np.linalg.svd(points - centroid)
+    direction_vector = vh[0]  # First right-singular vector is the principal direction
+    return centroid, direction_vector
+
+def bounding_box_interior_points(points):
+    bb_min=bounding_box.get_min_bound()
+    bb_max=bounding_box.get_max_bound()
+    p1=np.asarray(points.points)
+    inside_bbox = (p1 >= bb_min) & (p1 <= bb_max) #& for element-wise logic in numpy
+    within_bbox = np.all(inside_bbox, axis=1)
+    filtered_points=p1[within_bbox]
+    if filtered_points.size > 0:
+        #print( filtered_points[0] ) # Return the first point inside the bounding box
+        #print( filtered_points[-1] ) # Return the last point inside the bounding box
+        return(filtered_points[0],filtered_points[-1])
+    else:
+        print("No Points in Bounding Box")
+        return None 
+    
+def scan_width_determination(line_pcd1,line_pcd2):
+    p0_0,p0_1=bounding_box_interior_points(line_pcd1)
+    p1_0,p1_1=bounding_box_interior_points(line_pcd2)
+    scan_width=max(np.linalg.norm(p0_0-p1_0),np.linalg.norm(p0_1-p1_1))
+    return scan_width
 
 #Load the STL file
 #mesh = o3d.io.read_triangle_mesh("plane_segments\Circle_mesh.stl")
@@ -43,6 +73,23 @@ mesh.compute_adjacency_list()
 mesh.compute_triangle_normals()
 mesh.compute_vertex_normals()
 #o3d.visualization.draw_geometries([mesh], point_show_normal=False)
+
+#Take bounding box and find principal components
+bounding_box=mesh.get_oriented_bounding_box()
+bounding_box.color=([1,0,0])
+bblen=list(bounding_box.extent)
+primary_axis_index=bblen.index(max(bblen))
+popped_bblen=bblen[0:primary_axis_index]+bblen[primary_axis_index+1:]
+secondary_axis_index=bblen.index(max(popped_bblen))
+tertiary_axis_index=3-(primary_axis_index+secondary_axis_index)
+bb_len=bounding_box.extent[primary_axis_index]
+rot=bounding_box.R
+cent= bounding_box.center 
+primary_axis=bounding_box.R[:,primary_axis_index]
+secondary_axis=bounding_box.R[:,secondary_axis_index]
+tertiary_axis=bounding_box.R[:,tertiary_axis_index]
+
+
 
 # Find boundary edges and identify boundary verticies
 boundary = mesh.get_non_manifold_edges(allow_boundary_edges=False) #Non-manifold defined differently if True
@@ -62,7 +109,7 @@ Coords2D=boundary_vertices_coords[:,:2]
 hull = ConvexHull(Coords2D)
 hull_vertices=boundary_vertices_coords[hull.vertices]
 hull_vertices2D=Coords2D[hull.vertices]
-
+hull_vertices_list=[vertex.tolist() for vertex in hull_vertices]
 
 ##Calculate relative linearity of consecutive hull points (corners are angled)
 linscore=[]
@@ -101,14 +148,14 @@ for index, val in enumerate(hull_vertices2D):
 noise = np.random.uniform(1e-4, 1e-5, len(linscore))
 linscore=linscore+noise
 # Determine which coordinates are the corner points and plot
-paired=sorted(zip(linscore, hull_vertices2D)) #sort pairwise points by pairings
+paired=sorted(zip(linscore, hull_vertices)) #sort pairwise points by pairings
 re_ordered_pairs=[coord for score,coord in paired] #returns the pairs ordered from longest to shortest
 cornerpoints=re_ordered_pairs[:4]
 
 ## Determine what "edge" (aka between corners) aligns best with the primary scanning axis
 edges=list(itertools.combinations(cornerpoints, 2))
 edgescore=[]
-vec2=np.array([1,0,0]) #should be primary axis of point cloud
+vec2=primary_axis #should be primary axis of point cloud
 for index, val in enumerate(edges):
     curr_point=val
     vec1=curr_point[1]-curr_point[0]
@@ -116,16 +163,111 @@ for index, val in enumerate(edges):
 edges_lists=[[vertex.tolist() for vertex in edge] for edge in edges]
 paired=sorted(zip(edgescore, edges_lists), reverse=True) #sort pairwise points by pairings
 re_ordered_pairs=[edge for score,edge in paired] #returns the pairs ordered from longest to shortest
-alignededges=re_ordered_pairs[:2]
+aligned_edges=re_ordered_pairs[:2]
 
+## Take edge and classify all points between as point to best-fit 
+hull_vertices_list=[vertex.tolist() for vertex in hull_vertices]
+primary_edge=aligned_edges[0]
+secondary_edge=aligned_edges[1]
+group1=splitting(len(hull_vertices_list), primary_edge, hull_vertices_list)
+group2=splitting(len(hull_vertices_list), secondary_edge, hull_vertices_list)
 
+# Form point-cloud of fitting
+prop_len=int(bb_len/5)
+centroid1,vec1=fit_line_3d(group1)
+line_points1 = np.array([centroid1 + t * vec1 for t in np.linspace(-bb_len/2-prop_len, bb_len/2+prop_len, 100)])
+centroid2,vec2=fit_line_3d(group2)
+line_points2 = np.array([centroid2 + t * vec2 for t in np.linspace(-bb_len/2-prop_len, bb_len/2+prop_len, 100)])
+#print(vec1,vec2)
 
+line_pcd1 = o3d.geometry.PointCloud()
+line_pcd1.points = o3d.utility.Vector3dVector(line_points1)
+line_pcd1.paint_uniform_color([0, 1, 0])
+line_pcd2 = o3d.geometry.PointCloud()
+line_pcd2.points = o3d.utility.Vector3dVector(line_points2)
+line_pcd2.paint_uniform_color([0, 0, 1])
+'''
+#Code for Interpolating lines
+intermediate_points=o3d.geometry.PointCloud()
+n=5
+for i in range(1,n):
+   temp_ptc=o3d.geometry.PointCloud()
+   temp_ptc.points=o3d.utility.Vector3dVector(np.asarray(line_pcd1.points)*(1-i/(n-1))+np.asarray(line_pcd2.points)*(i/(n-1)))
+   temp_ptc.paint_uniform_color([0, 1-i/(n-1),i/(n-1)])
+   intermediate_points+=temp_ptc
 
+o3d.visualization.draw_geometries([segment.mesh, segment.bounding_box, boundary_pcd, line_pcd1, line_pcd2, intermediate_points])
+'''
 
+probe_width=64
+half_probe=probe_width/2
+tot_width=scan_width_determination(line_pcd1,line_pcd2)
+print(f"Total width to be scanned: {tot_width}")
 
+#setting both edges
+offset_one=probe_width/2
+offset_two=tot_width-offset_one
+tot_passes=math.ceil(tot_width/probe_width)
+per_pass_width=round(tot_width/tot_passes,2)
+print(f"Width allocated to each pass: {per_pass_width}")
 
+real_distance=tot_width-2*probe_width
+passes_left=math.ceil(real_distance/probe_width)
+real_per_pass_width=real_distance/passes_left
+print(f"Number of total passes: {tot_passes}")
+print(f"Number of passes remaining after setting edges: {passes_left}")
+print(f"Approximate pass width after setting edges: {real_per_pass_width}")
 
+interp_one=probe_width+real_per_pass_width/2
+interp_two=(tot_width-probe_width)-real_per_pass_width/2
 
+#Edge Offset: find what direction to move in for each vector
+#print(direction1/np.linalg.norm(direction1), segment.secondary_axis)
+#print(np.dot(direction1/np.linalg.norm(direction1),segment.secondary_axis))
+#print(np.dot(direction2/np.linalg.norm(direction2),segment.secondary_axis))
+direction1=centroid1-cent
+direction2=centroid2-cent
+dot_1=np.dot(direction1/np.linalg.norm(direction1),secondary_axis)
+dot_2=np.dot(direction2/np.linalg.norm(direction2),secondary_axis)
+
+#shift_vec=1/2 probe width shift in the direction of the secondary-axis
+shift_vec=half_probe*secondary_axis
+if dot_1<0:
+    dot_1=-1
+    start=1
+else:
+    dot_1=1 
+    start=0 #should build the opposite way
+
+#Shift the Vectors 1/2 probe width
+line_one=np.asarray(line_pcd1.points) - dot_1*shift_vec
+line_last=np.asarray(line_pcd2.points) - dot_2*shift_vec
+color_one=np.asarray([0, 1, 0])
+color_two=np.asarray([0, 0, 1])
+'''
+#Option 1 (Option 2 from Num_pass_calc - Intermediate Interpolation) -NOT Prefered
+interp_one=np.asarray(line_pcd1.points) - dot_1*(probe_width+real_per_pass_width/2)*segment.secondary_axis
+interp_two=np.asarray(line_pcd2.points) - dot_2*(probe_width+real_per_pass_width/2)*segment.secondary_axis
+lines=[]
+for i in range(tot_passes):
+    if i==0:
+        lines.append(line_one)
+    elif i==tot_passes-1:
+        lines.append(line_last)
+    else:
+        lines.append(interp_one*(1-(i-1)/(passes_left-1))+interp_two*((i-1)/(passes_left-1)))
+'''
+#Option 2 (Option 3 from Num_pass_calc - Direct interpolation)
+lines=[]
+color=[]
+for i in range(tot_passes):
+    lines.append(line_one*(1-(i)/(tot_passes-1))+line_last*((i)/(tot_passes-1)))
+    color.append(color_one*(1-(i)/(tot_passes-1))+color_two*((i)/(tot_passes-1)))
+color_arrays = [np.array(color) * np.ones((lines[1].shape[0], 1)) for points, color in zip(lines, color)]
+trial=o3d.geometry.PointCloud()
+trial.points=o3d.utility.Vector3dVector(np.vstack(lines))
+trial.colors=o3d.utility.Vector3dVector(np.vstack(color_arrays))
+o3d.visualization.draw_geometries([mesh, bounding_box, trial])
 
 
 
