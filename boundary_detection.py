5563e6c760e4e2df4903eefe3eacfd565929d9fa
Jonas <jbeachy@uw.edu>
2025-03-10 21:27:10 -0700

    integrated change in process involving edge dection and edge fitting

--- boundary_detection.py
+++ boundary_detection.py
@@ -3,12 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PCAClass import PCABounding, Best_Fit_CPP
 import time
-import math
+
 
 #To see the called functions in the Best_Fit_CPP class, see PCAClass.py
-
-s=time.time()
-
+start=time.time()
 #Import Mesh here!
 #segment=Best_Fit_CPP("plane_segments\plane_segment_8_mesh.stl")
 #segment=Best_Fit_CPP("plane_segments\plane_segment_1_mesh.stl")
@@ -16,132 +14,84 @@ s=time.time()
 #segment=Best_Fit_CPP("plane_segments\Hyper_meshed_noise.stl")
 #segment=Best_Fit_CPP(r"plane_segments\Non-planar.stl")
 #segment=Best_Fit_CPP(r"plane_segments\Bowl.stl")
-segment=Best_Fit_CPP("plane_segments\Foil.stl")
-#segment=Best_Fit_CPP("plane_segments\Concave.stl")
+#segment=Best_Fit_CPP("plane_segments\Foil.stl")
+segment=Best_Fit_CPP("plane_segments\Concave.stl")
 #segment=Best_Fit_CPP("plane_segments\Convex.stl")
+#segment=Best_Fit_CPP("plane_segments\BigL.stl")
+#segment=Best_Fit_CPP("plane_segments\Curvy.stl")
+#segment=Best_Fit_CPP("plane_segments\Curvy.stl")
+#segment=Best_Fit_CPP("plane_segments\surjective_xz.stl")
 
 
 # Ensure the mesh has edges and triangle information for visualization
 segment.mesh.compute_adjacency_list()
 segment.mesh.compute_triangle_normals()
 segment.mesh.compute_vertex_normals()
+segment.mesh.paint_uniform_color([0.5, 0.5, 0.5])
+#print(f"Principal Axes:\n {segment.PCA_eigenvecs}")
+#print(f"Relative Importance:\n {segment.PCA_eigenvals}")
 
-print(f"Principal Axes:\n {segment.PCA_eigenvecs}")
-print(f"Relative Importance:\n {segment.PCA_eigenvals}")
-
-# Find boundary edges and identify boundary verticies
-boundary = segment.mesh.get_non_manifold_edges(allow_boundary_edges=False) #Non-manifold defined differently if True
-#boundary=segment.boundary_edge_finder()
-boundary_vertices = np.unique(np.array(boundary).flatten())
-segment.boundary_vertices_coords = np.asarray(segment.mesh.vertices)[boundary_vertices]
+segment.boundary_edge_calculations()
 
 # Create a visualization object
 boundary_pcd=o3d.geometry.PointCloud()
-boundary_pcd.points = o3d.utility.Vector3dVector(segment.boundary_vertices_coords)
+boundary_pcd.points = o3d.utility.Vector3dVector(segment.ordered_edge_points)
 boundary_pcd.paint_uniform_color([1,0,0])
 
-# Display bounding-box coordinate frame (from PCA)
-transform=np.eye(4)
-PCA_coord_frame=o3d.geometry.TriangleMesh.create_coordinate_frame(size=100.0)
-transform[:3,:3]=segment.rot
-transform[:3,3]=segment.cent
-PCA_coord_frame.transform(transform)
-#o3d.visualization.draw_geometries([segment.mesh,segment.bounding_box, PCA_coord_frame], point_show_normal=False)
+## Display bounding-box coordinate frame (from PCA)
+#transform=np.eye(4)
+#PCA_coord_frame=o3d.geometry.TriangleMesh.create_coordinate_frame(size=100.0)
+#Not positive-ized axes
+#transform[:3,:3]=segment.PCA_eigenvecs
+#transform[:3,3]=segment.cent+10*segment.PCA_eigenvecs[segment.tertiary_axis_index]
+#PCA_coord_frame.transform(transform)
+#o3d.visualization.draw_geometries([segment.mesh,segment.bounding_box, PCA_coord_frame], mesh_show_back_face=True)
+#print(transform)
 
-#Take convex hull and find the verticies
-#Find_convex_hull projects into 2D along least PCA axis, and takes hull of 2D points
-segment.find_convex_hull(2, segment.boundary_vertices_coords)
-#Boundary2D=segment.PCA_projection(2,segment.boundary_vertices_coords)
 
-#Compare linearity of groups of 3 consecutive hull points. 
-#Least linear groups are centered on corner point
-segment.find_corner_points()
-#print(segment.hull_vertices)
 
-# #2D plot of convex hull and corner points
-plt.plot(segment.PCA_pointsND[:,segment.primary_axis_index], segment.PCA_pointsND[:,segment.secondary_axis_index], 'o', label='Edge points')
-plt.plot(segment.hull_verticesND[:,segment.primary_axis_index],segment.hull_verticesND[:,segment.secondary_axis_index], "r*",markersize=10,label="Convex Hull")
-plt.plot(np.array(segment.corner_pointsND)[:,segment.primary_axis_index],np.array(segment.corner_pointsND)[:,segment.secondary_axis_index], 'y*',markersize=20,label='Corner Points')
-plt.xlabel("Principle Axis", fontweight="bold",fontsize=14)
-plt.ylabel("Secondary Axis", fontweight="bold",fontsize=14)
-plt.legend(prop={'size': 14, 'weight': 'bold'})
-plt.xticks(fontsize=11, fontweight='bold')
-plt.yticks(fontsize=11, fontweight='bold')
-plt.show()
+# # #2D plot of convex hull and corner points
+# plt.plot(segment.ordered_edge_points2D[:,segment.primary_axis_index], segment.ordered_edge_points2D[:,segment.secondary_axis_index], 'o', label='Edge points')
+# plt.plot(np.array(segment.corner_points2D)[:,segment.primary_axis_index],np.array(segment.corner_points2D)[:,segment.secondary_axis_index], 'y*',markersize=20,label='Corner Points')
+# plt.xlabel("Principle Axis", fontweight="bold",fontsize=14)
+# plt.ylabel("Secondary Axis", fontweight="bold",fontsize=14)
+# plt.legend(prop={'size': 14, 'weight': 'bold'})
+# plt.xticks(fontsize=11, fontweight='bold')
+# plt.yticks(fontsize=11, fontweight='bold')
+# plt.show()
 
-## Determine what "edge" (aka between corners) aligns best with the primary scanning axis
-segment.find_primary_scanning_edges()
 
 ## Take edge and classify all points between as point to best-fit 
-hull_vertices_list=[vertex.tolist() for vertex in segment.hull_vertices]
-primary_edge=segment.aligned_edges[0]
-secondary_edge=segment.aligned_edges[1]
-group1=segment.splitting(primary_edge, hull_vertices_list)
-group2=segment.splitting(secondary_edge, hull_vertices_list)
+segment.edge_fitter(segment.edges)
 
-print()
-#Find the 1D fitting (line) of points
-segment.edge1_cent,segment.edge1_vec=segment.fit_line_3d(group1)
-segment.edge2_cent,segment.edge2_vec=segment.fit_line_3d(group2)
+#Determine scanning width and required pass info
+Probe_width=64
+segment.scan_information(Probe_width)
 
-
-
-#Form the array of points that make up the line
-line_points1 = segment.point_creator(segment.edge1_cent,segment.edge1_vec, 100)
-line_points2 = segment.point_creator(segment.edge2_cent,segment.edge2_vec, 100)
-
-if np.linalg.norm(line_points1[0]-line_points2[0])>np.linalg.norm(line_points1[0]-line_points2[-1]):
-    line_points2=line_points2[::-1]
-
-
-# # Visualization of interpolated lines
-# line_pcd1 = o3d.geometry.PointCloud()
-# line_pcd1.points = o3d.utility.Vector3dVector(line_points1)
-# line_pcd1.paint_uniform_color([0, 1, 0])
-# line_pcd2 = o3d.geometry.PointCloud()
-# line_pcd2.points = o3d.utility.Vector3dVector(line_points2)
-# line_pcd2.paint_uniform_color([0, 0, 1])
-# #Code for Interpolating lines
-# intermediate_points=o3d.geometry.PointCloud()
-# n=5
-# for i in range(1,n):
-#    temp_ptc=o3d.geometry.PointCloud()
-#    temp_ptc.points=o3d.utility.Vector3dVector(np.asarray(line_pcd1.points)*(1-i/(n-1))+np.asarray(line_pcd2.points)*(i/(n-1)))
-#    temp_ptc.paint_uniform_color([0, 1-i/(n-1),i/(n-1)])
-#    intermediate_points+=temp_ptc
-
-# o3d.visualization.draw_geometries([segment.mesh, segment.bounding_box, boundary_pcd, line_pcd1, line_pcd2, intermediate_points])
-
-
-#Scan_information takes: probe width, scan_line1, scan_line2, and the edge offset (optional)
-segment.scan_information(64, line_points1, line_points2,)
 #Prints out relevant information about scan_passes
 segment.print_scan_information()
 
 #Edge Offset: find what direction to move in for each vector
 segment.shift_direction()
-#shift_vec=offset (default is 1/2 probe width) shift in the direction of the secondary-axis
-shift_vec=segment.offset_one*segment.secondary_axis
-line_one=np.asarray(line_points1) - segment.offset_dir*shift_vec
-line_last=np.asarray(line_points2) + segment.offset_dir*shift_vec
 
-#Get official lines returned; with consideration for number of passes
-lines,color=segment.line_interpolator(line_one,line_last)
-color_arrays = [np.array(color) * np.ones((lines[0].shape[0], 1)) for points, color in zip(lines, color)]
+#shift_vec
+shift_vec=segment.offset_one*segment.secondary_axis
+segment.edge1_CP=segment.edge1_CP - segment.offset_dir*shift_vec
+segment.edge2_CP=segment.edge2_CP + segment.offset_dir*shift_vec
+
+#Interpolate Bézier Curves
+passes,colors=segment.line_interpolator(10)
 
 #Add 10 mm to each point's z-coordinate in the trial lines; TODO: add triangle normal component
-adjusted_lines = [line + np.array([0, 0, 10]) for line in lines]
-e=time.time()
-print(f"{round(e-s,3)} seconds run time")
-
-#Path visualization
-trial=o3d.geometry.PointCloud()
-trial.points=o3d.utility.Vector3dVector(np.vstack(adjusted_lines))
-trial.colors=o3d.utility.Vector3dVector(np.vstack(color_arrays))
-o3d.visualization.draw_geometries([segment.mesh,segment.bounding_box, trial, boundary_pcd ],mesh_show_back_face=True)
+adjusted_lines = np.vstack(passes)+ np.array([0, 0, 10])
 
 
-s2=time.time()
+# #Path visualization
+# trial=o3d.geometry.PointCloud()
+# trial.points=o3d.utility.Vector3dVector(adjusted_lines)
+# trial.colors=o3d.utility.Vector3dVector(np.vstack(colors))
+# o3d.visualization.draw_geometries([segment.mesh,segment.bounding_box, trial, boundary_pcd ],mesh_show_back_face=True)
+
 
 '''
 # Define probe dimensions
@@ -155,7 +105,7 @@ e1=time.time()
 o3d.visualization.draw([scanned_mesh, trial], show_ui=True)
 print(f"Scan calculation time: {e1-s1}")
 '''
-
+start2=time.time()
 #Format mesh for ray-casting
 tensor_plane = o3d.t.geometry.TriangleMesh.from_legacy(segment.mesh) #OGplane= Surface to Path-Plan on
 tensor_plane.compute_triangle_normals()
@@ -167,10 +117,12 @@ poses=[]
 names=[]
 
 #direction for ray-casting should be along tertiary axis, but pointing "down"
-direction=np.array(segment.tertiary_axis) * np.ones((lines[0].shape[0], 1))*-1
-for i, point_set in enumerate(lines):
+unit_direction=np.array(segment.tertiary_axis)*-1
+
+for i, point_set in enumerate(passes):
+    direction = unit_direction * np.ones((point_set.shape[0], 1))
     #move points "up" on z axis by max z bb height to ensure projections have intersections
-    z_offset=segment.bounding_box.extent[-1]*segment.tertiary_axis
+    z_offset=min(segment.bounding_box.extent)*segment.tertiary_axis
     ray_origins=point_set+z_offset
     #Ray Cast to test for intersection
     LocVec=np.hstack((ray_origins.astype(dtype=np.float32), direction.astype(np.float32)))
@@ -236,8 +188,9 @@ for i, point_set in enumerate(lines):
 
 
 segment.mesh.vertex_colors = o3d.utility.Vector3dVector(segment.colors)
-e2=time.time()
-print(f"total time for checking: {e2-s2}")
+end=time.time()
+print(f"total time for checking: {end-start2}")
+print(f"Total Path-Planning runtime: {end-start}")
 o3d.visualization.draw_geometries([segment.mesh],mesh_show_back_face=True)
 
 
